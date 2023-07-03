42b133be2a017720deb328b558a9fbd3885b1f87
Aman Satyawali <aman.satyawali@tigeranalytics.com>
2023-07-03 18:12:50 +0530

    added code for blob write

--- blob_access.py
+++ blob_access.py
@@ -4,13 +4,35 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 account_url = "https://amancloudtrainingstorage.blob.core.windows.net"
 default_credential = DefaultAzureCredential()
+container_name = "container1"
 
 blob_service_client = BlobServiceClient(account_url, credential=default_credential)
 
 try:
     print("Azure Blob Storage Python quickstart sample")
 
-                # Quickstart code goes here
+    # Create a local directory to hold blob data
+    local_path = "./data"
+    os.mkdir(local_path)
+
+    # Create a file in the local data directory to upload and download
+    local_file_name = str(uuid.uuid4()) + ".txt"
+    upload_file_path = os.path.join(local_path, local_file_name)
+
+    # Write text to the file
+    file = open(file=upload_file_path, mode='w')
+    file.write("Hello, World!")
+    file.close()
+
+    # Create a blob client using the local file name as the name for the blob
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+
+    print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+
+    # Upload the created file
+    with open(file=upload_file_path, mode="rb") as data:
+        blob_client.upload_blob(data)
+
 except Exception as ex:
     print('Exception:')
     print(ex)
