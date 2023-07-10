bd37eb6aa0b7a4110c753be661b4127c9a4b01c9
Aman Satyawali <aman.satyawali@tigeranalytics.com>
2023-07-10 11:54:35 +0530

    changed port for flask app to test

--- home.py
+++ home.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello FLSK APP RUNNING!"
+
+
+if __name__ == '__main__':
+    app.run(host=0.0.0.0, port=8000, debug=True)
